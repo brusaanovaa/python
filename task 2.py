a2b8e26829d84c5a33fd8f4df330879ba1e501f5
brusaanovaa <vmakkenziv@yandex.ru>
2022-03-01 14:51:34 +0300

    task 2 + file

--- task 2.py
+++ task 2.py
@@ -1,4 +1,4 @@
-with open(r"C:\Users\DELL\Downloads\rus.txt", "r", encoding='utf-8') as file_obj:
+with open(r"C:\Users\DELL\PycharmProjects\python\rus.txt", "r", encoding='utf-8') as file_obj:
     lines = 0
     letters = 0
 
@@ -6,5 +6,5 @@ with open(r"C:\Users\DELL\Downloads\rus.txt", "r", encoding='utf-8') as file_obj
         lines += 1
         letters = len(line.split())
 
-print(f"Кол-во слов в строке = {letters}")
+print(f"Кол-во слов/символов в строке = {letters}")
 print(f"Кол-во строк = {lines}")
